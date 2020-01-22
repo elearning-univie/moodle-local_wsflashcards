ef200333a64ac082853f9270cb9ec40c161fd957
Adrian Czermak <adrian.czermak@univie.ac.at>
2020-01-22 15:55:10 +0100

    init commit

---
+++ db/services.php
@@ -0,0 +1,67 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * External service definition
+ *
+ * @package    local_wsflashcards
+ * @copyright  2020 University of Vienna
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+const LOCAL_WSFLASHCARDS_SERVICE_SHORTNAME = 'wsflashcards';
+
+$services = array(
+        'wsflashcards' => array(
+                'functions' => array(),
+                'shortname' => LOCAL_WSFLASHCARDS_SERVICE_SHORTNAME,
+                'requiredcapability' => 'mod/flashcards:webservice',
+                'restrictedusers' => 0,
+                'enabled' => 1,
+        )
+);
+
+$functions = array(
+        'wsflashcards_get_courses' => array(
+                'classname'   => 'local_wsflashcards_external',
+                'methodname'  => 'get_courses',
+                'classpath'   => 'local/wsflashcards/externallib.php',
+                'description' => 'get_courses',
+                'type'        => 'read',
+                'loginrequired' => true,
+                'services' => array('wsflashcards')
+        ),
+        'wsflashcards_get_questions' => array(
+                'classname'   => 'local_wsflashcards_external',
+                'methodname'  => 'get_questions',
+                'classpath'   => 'local/wsflashcards/externallib.php',
+                'description' => 'get_questions',
+                'type'        => 'read',
+                'loginrequired' => true,
+                'services' => array('wsflashcards')
+        ),
+        'wsflashcards_set_answers' => array(
+                'classname'   => 'local_wsflashcards_external',
+                'methodname'  => 'set_answers',
+                'classpath'   => 'local/wsflashcards/externallib.php',
+                'description' => 'set_answers',
+                'type'        => 'write',
+                'loginrequired' => true,
+                'services' => array('wsflashcards')
+        )
+);
---
+++ externallib.php
@@ -0,0 +1,130 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * External service definition
+ *
+ * @package    local_wsflashcards
+ * @copyright  2020 University of Vienna
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once("$CFG->libdir/externallib.php");
+
+/**
+ * Class local_wsflashcards_external
+ *
+ * @copyright  2020 University of Vienna
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class local_wsflashcards_external extends external_api {
+
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     */
+    public static function get_courses_parameters() {
+        return new external_function_parameters(
+                array()
+        );
+    }
+
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     */
+    public static function get_questions_parameters() {
+        return new external_function_parameters(
+                array()
+        );
+    }
+
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     */
+    public static function set_answers_parameters() {
+        return new external_function_parameters(
+                array(
+                        'flashcardsid' => new external_value(PARAM_INT, 'id of activity')
+                )
+        );
+    }
+
+    /**
+     * Moves the question into the next box if the answer was correct, otherwise to box 1
+     * @return int
+     * @throws dml_exception
+     */
+    public static function get_courses() {
+        return 2;
+    }
+
+    /**
+     * Moves all questions from box 0 to box 1 for the activity
+     *
+     * @return int
+     * @throws coding_exception
+     * @throws dml_exception
+     */
+    public static function get_questions() {
+        return 1;
+    }
+
+    /**
+     * Moves all questions from box 0 to box 1 for the activity
+     *
+     * @param int $flashcardsid
+     * @return int
+     * @throws coding_exception
+     * @throws dml_exception
+     */
+    public static function set_answers($flashcardsid) {
+        return $flashcardsid;
+    }
+
+    /**
+     * Returns return value description
+     *
+     * @return external_value
+     */
+    public static function get_courses_returns() {
+        return new external_value(PARAM_INT, 'new question');
+    }
+
+    /**
+     * Returns return value description
+     *
+     * @return external_value
+     */
+    public static function get_questions_returns() {
+        return new external_value(PARAM_INT, 'new question');
+    }
+
+    /**
+     * Returns return value description
+     *
+     * @return external_value
+     */
+    public static function set_answers_returns() {
+        return new external_value(PARAM_INT, 'new question');
+    }
+}
