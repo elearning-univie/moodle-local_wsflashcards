2e8fe4e8480107c2d28878692a6165a11799e8e2
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2020-02-25 09:17:55 +0100

    test helper

--- tests/wsclientlib.php
+++ tests/wsclientlib.php
@@ -1,2 +1,56 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * wsflashcards test helper functions
+ *
+ * @package    local_wsflashcards
+ * @copyright  2020 University of Vienna
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined(MOODLE_INTERNAL) || die();
+
+function callws($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    $result = curl_exec($ch);
+    
+    curl_close($ch);
+    return $result;
+}
+
+function array2string($data,$momentstring){
+    $result = "";
+    foreach ($data as $key => $value) {
+        if (is_array($value)) {
+            if($momentstring) {
+                $newmoment = $momentstring . '[' . $key . ']';
+                $result .= array2string($value, $newmoment);
+            } else {
+                $result .= array2string($value, $key);
+            }
+        } else {
+            $result .= "&$momentstring". "[$key]" . "=$value";
+        }
+    }
+    return $result;
+}
--- tests/wsflashcards_get_questions.php
+++ tests/wsflashcards_get_questions.php
@@ -1,22 +1,32 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * test for get_questions function
+ *
+ * @package    local_wsflashcards
+ * @copyright  2020 University of Vienna
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 define('CLI_SCRIPT', true);
 require(__DIR__.'/../../../config.php');
 global $CFG;
 require_once($CFG->libdir.'/clilib.php');
-
-function callws($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    $result = curl_exec($ch);
-    
-    curl_close($ch);
-    return $result;
-}
+require('wsclientlib.php');
 
 list($options, $unrecognised) = cli_get_params([
     'help' => false,
