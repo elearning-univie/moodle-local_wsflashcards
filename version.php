d382a86f6337c8c83a72c4727d82d220c4172d07
Adrian Czermak <adrian.czermak@univie.ac.at>
2020-03-12 12:36:39 +0100

    new version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wsflashcards';
-$plugin->version = 2020031000;
+$plugin->version = 2020031200;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('mod_flashcards' => 2020012000);
